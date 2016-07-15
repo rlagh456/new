0c2d64145d18861967e525de8f76cba46838fbc7
Administrator <Administrator@dfa-dfa>
2016-07-15 16:24:05 +0900

    1차수정

---
+++ new1/src/hello.java
@@ -0,0 +1,9 @@
+package src;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("È£¿ì");
+	}
+
+}
---
+++ new1/src/hello2.java
@@ -0,0 +1,10 @@
+package src;
+
+public class hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("호호호우");
+
+	}
+
+}
